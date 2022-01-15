78fa849724fa85cddd54953730d9deaa0e7c2847
IvanBelsky <mostygas@tut.by>
2022-01-15 14:55:23 +0300

    add new directory + new 2 classes + bundle profiler+sensio

---
+++ src/DataOperations/DataManager/UserDataManager.php
@@ -0,0 +1,23 @@
+<?php
+//Файл используется для: добавления, удаления, обновления
+namespace App\DataOperations\DataManager;
+
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserDataManager
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function delete(User $user): void
+    {
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+    }
+}
