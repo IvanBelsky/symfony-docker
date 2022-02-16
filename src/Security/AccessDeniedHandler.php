db139529d8cff6cf06f21026fce5613f5638a2a1
IvanBelsky <mostygas@tut.by>
2022-02-16 14:34:33 +0300

    Fixed nginx configuration in docker container (502)

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,19 @@
+<?php
+/*403 Forbidden response */
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        // ...
+
+        return new Response('Доступ запрещен!, 403');
+    }
+}
