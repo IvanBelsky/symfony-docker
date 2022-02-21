1085408855674f22e2a4c4d6661c58a9910179a3
IvanBelsky <mostygas@tut.by>
2022-02-21 19:19:43 +0300

    Continue add fixtures (comment&iplog)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,13 @@
 
 namespace App\DataFixtures;
 
-use App\DataOperations\DataProvider\UserDataProvider;
 use App\Entity\Address;
 use App\Entity\Articles;
+use App\Entity\Comment;
 use App\Entity\User;
+use App\Entity\UserIpLog;
+use App\Repository\ArticlesRepository;
+use App\Repository\UserIpLogRepository;
 use App\Repository\UserRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -14,24 +17,30 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class AppFixtures extends Fixture
 {
     private $hasher;
-    private $userCount;
-    private $userDataProvider;
+ //   private $userCount;
+  //  private $articlesCount;
     private $userRepository;
+    private $articlesRepository;
+ //   private $iplogRepository;
     protected $firstnameArray = ['John', 'Sarah', 'Jane', 'Michael', 'Steven', 'Rachel'];
     protected $surnameArray = ['Smith', 'Mills', 'Baker', 'Jones', 'Macher', 'Doe'];
     protected $townArray = ['Minsk', 'Moscow', 'Tokyo', 'Mosty', 'London', 'New-York','Berlin'];
     protected $streetArray = ['Lermontova', 'Pushkina', 'Yantarnaya','Mira', 'Yunosty','Gogoliya','Zelenaya'];
-    protected $articleName1 = ['Компания Sonatic ','Microsoft ','Tesla ', 'Nokia ', ];
-    protected $articleName2 = ['разрабатывает ', 'начанает производство ','представляет ','выводит на рынок','завершает '];
+    protected $articleName1 = ['компания Sonatic ','Microsoft ','Tesla ', 'Nokia ', ];
+    protected $articleName2 = ['разрабатывает ', 'начанает производство ','представляет ','выводит на рынок ','завершает '];
     protected $articleName3 = ['новый софт ','автомобиль на батарейках ','новый продукт ','новую технологию передачи чего-то ','всё '];
     protected $words = ['несмотря ', 'на ', 'то,', 'что ', 'технологии ', 'искусственного ', 'интеллекта ',
                         'компания ', 'разработала ', 'алгоритм ','развиваются ', 'очень ', 'быстро '];
+    private $allWords = [];
 
-    public function __construct(UserPasswordHasherInterface $hasher, UserDataProvider $userDataProvider, UserRepository $userRepository)
+    public function __construct(UserPasswordHasherInterface $hasher,  UserRepository $userRepository,
+        ArticlesRepository $articlesRepository) //, UserIpLogRepository $ipLogRepository)
     {
         $this->hasher = $hasher;
-        $this->userDataProvider = $userDataProvider;
         $this->userRepository = $userRepository;
+        $this->articlesRepository = $articlesRepository;
+      //  $this->iplogRepository = $ipLogRepository;
+        $this->allWords = array_merge($this->words, $this->articleName1, $this->articleName2,$this->articleName3);
     }
 
     public function load(ObjectManager $manager): void
@@ -58,8 +67,8 @@ class AppFixtures extends Fixture
         //*****************************************************************************
        // $this->userCount = count($this->userDataProvider->getAllUsers());
         $listUsers = $this->userRepository->findAll();
-        $this->userCount = count($listUsers);
-        for ($i = 0; $i<$this->userCount; $i++)
+        $userCount = count($listUsers);
+        for ($i = 0; $i<$userCount; $i++)
         {
             $address = new Address();
             $address->setUser($listUsers[$i]);
@@ -72,11 +81,10 @@ class AppFixtures extends Fixture
         }
         $manager->flush();
         //***************************************************************************
-        for ($i = 0; $i<$this->userCount*3; $i++)
+        for ($i = 0; $i<$userCount*3; $i++)
         {
             $articles = new Articles();
-         //   $articles->setUser($listUsers[0]);
-            $articles->setUser($listUsers[random_int(0,$this->userCount-1)]);
+            $articles->setUser($listUsers[random_int(0,$userCount-1)]);
             $articles->setName($this->customArticleName());
             $articles->setFoto('img'.$i.'.jpeg');
             $articles->setTextArt($this->customArticleText());
@@ -84,7 +92,44 @@ class AppFixtures extends Fixture
         }
         $manager->flush();
         //***************************************************************************
+        $listArticles = $this->articlesRepository->findAll();
+        $articlesCount = count($listArticles);
+        $date = new \DateTime();
+        for ($i = 0; $i<$articlesCount*3; $i++)
+        {
+            $comment = new Comment();
+            $comment->setUser($listUsers[random_int(0,$userCount-1)]);
+            $comment->setContent($this->customArticleName());
+            $st = 'P'.random_int(0,12).'M'.random_int(0,28).'DT4H3M2S';
+            $interval = new \DateInterval($st);
+            $comment->setDateCreated( $date->sub($interval));
+            $comment->setArticles($listArticles[random_int(0,$articlesCount-1)]);
+            $manager->persist($comment);
+        }
+        $manager->flush();
 
+        //**************************************************************************
+      //  $date = new \DateTime();
+        for ($i = 0; $i<$userCount*3; $i++)
+        {
+            $ipLog = new UserIpLog();
+            $ipLog->setUser($listUsers[random_int(0,$userCount-1)]);
+            $ipLog->setIpAdr($this->customIp());
+            $st = 'P'.random_int(0,12).'M'.random_int(0,28).'DT1H10M15S';
+            $interval = new \DateInterval($st);
+            $ipLog->setDateCreated( $date->sub($interval));
+            $ipLog->setId2(0);
+            $manager->persist($ipLog);
+        }
+        $manager->flush();
+
+        //**************************************************************************
+
+    }
+
+    public function customIp()
+    {
+       return random_int(1,192).'.'.random_int(1,168).'.'.random_int(1,254).'.'.random_int(1,254);
     }
 
     public function customName()
@@ -109,17 +154,20 @@ class AppFixtures extends Fixture
 
     public function customArticleName()
     {
-        return $this->articleName1[rand(0, count($this->articleName1) - 1)]
+        $this->rus();
+        return $this->mb_ucfirst(
+         $this->articleName1[rand(0, count($this->articleName1) - 1)]
             .$this->articleName2[rand(0, count($this->articleName2) - 1)]
-            .$this->articleName3[rand(0, count($this->articleName3) - 1)];
+            .$this->articleName3[rand(0, count($this->articleName3) - 1)]
+        );
     }
     public function customArticleText()
     {
-        $n = count($this->words);
+        $n = count($this->allWords);
         $n1 = random_int($n,$n+20);
         $text = '';
         for ($i=0; $i<$n1; $i++) {
-            $text = $text.$this->words[random_int(0,$n-1)];
+            $text = $text.$this->allWords[random_int(0,$n-1)];
         }
 
         //return ucfirst($text);
@@ -127,7 +175,7 @@ class AppFixtures extends Fixture
         return $this->mb_ucfirst($text);
     }
 
-    //************************
+//************************
 public function rus()
 {
     /**
@@ -137,10 +185,8 @@ public function rus()
     if(!function_exists('mb_ucfirst') && extension_loaded('mbstring')) {
         /**
          * mb_ucfirst - преобразует первый символ в верхний регистр
-         *
          * @param string $str      - строка
          * @param string $encoding - кодировка, по-умолчанию UTF-8
-         *
          * @return string
          */
     }
